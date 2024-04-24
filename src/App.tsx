539991566314cd1f48f17535841f4d96f437c35b
dylan <scmiledrudge@gmail.com>
2024-04-24 10:33:51 -0700

    wow

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import logo from "./logo.svg";
+import {
+  ComponentValuesType,
+  VideoViewer,
+} from "@adept-at/lib-react-components";
+import "./App.css";
 
 function App() {
+  const ref = useRef<HTMLVideoElement>(null);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <VideoViewer
+          videoRef={ref}
+          componentValues={{
+            addedAt: "2020-12-17T00:44:23.174Z",
+            encodingId: null,
+            encodingProvider: null,
+            encodingStatus: null,
+            muxKey: "a80c55a7-d852-4756-89f5-4184f189b45f",
+            order: 300,
+            playbackUrl:
+              "https://stream.mux.com/01b2r4H6Pg8Q01NJZGppCu6X6tmfP6f6Jtp5oFZaETUwU.m3u8?redundant_streams=true",
+            title: null,
+            updatedAt: "2020-12-17T00:45:52.474Z",
+            uploadedFileName: "recording uploaded",
+            videoId: "1031e6e9-b85c-473b-beaf-1223761b1971",
+            type: ComponentValuesType.Video,
+          }}
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
